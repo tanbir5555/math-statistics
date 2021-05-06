a2f33b5f05bee69e6f11efa907f5924ea9d619f8
tanbir5555 <tanbirhawlader12690@gmail.com>
2021-05-07 05:25:35 +0600

    Successful in Function components

--- components/app/components/table.js
+++ components/app/components/table.js
@@ -1,64 +1,37 @@
 import React from "react";
 import './style/table.css'
-export default function Table() {
+export default function Table({highClass,lowClass,frequency,classes,midPoints}) {
   return (
     <div className="container">
       <table id="customers">
+      <tbody>
+          
+       
         <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
+          <td><strong>classes</strong></td>
+          <td><strong>frequency</strong></td>
+          <td><strong>highClass</strong></td>
+          <td><strong>lowClass</strong></td>
+          <td><strong>midPoints</strong></td>
+          {/* <td><strong></strong></td> */}
         </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Berglunds snabbköp</td>
-          <td>Christina Berglund</td>
-          <td>Sweden</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-        <tr>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Königlich Essen</td>
-          <td>Philip Cramer</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
-        <tr>
-          <td>North/South</td>
-          <td>Simon Crowther</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Paris spécialités</td>
-          <td>Marie Bertrand</td>
-          <td>France</td>
-        </tr>
+
+{classes.map((value,index)=>{
+
+  return(
+    <tr key={index}>
+          <td>{value}</td>
+          <td>{frequency[index]}</td>
+          <td>{lowClass[index]}</td>
+          <td>{highClass[index]}</td>
+          <td>{midPoints[index]}</td>
+          
+      </tr>
+  )
+})}
+        
+        
+       </tbody>
       </table>
     </div>
   );
--- components/app/main.js
+++ components/app/main.js
@@ -1,151 +1,86 @@
-import React, { Component } from "react";
-import Table from './components/table'
+import React, { useState } from "react";
+import Table from "./components/table";
 import Input from "./components/input";
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ini_data: {
-        classes: "",
-        frequency: "",
+function Main() {
+  let [classes, setClasses] = useState("");
+  let [highClass, setHighClass] = useState("");
+  let [lowClass, setLowClass] = useState("");
+  let [frequency, setFrequency] = useState("");
+  let [midPoints, setMidPoints] = useState("");
+  let [ready, setReady]=useState(false);
 
-      },
-      data: {
-        tableClass:[],
-        classesUpper: [],
-        classesLower: [],
-        frequency: [],
-        middle:[],
-        fX:[],
-        A:0,
-        d:[],
-        sumFrequency:0,
-        sumMiddle:0,
-        sumD:0,
-
-      },
-      table:<Table/>
-    };
-    this.updateFrequency=this.updateFrequency.bind(this)
-    this.updateClasses=this.updateClasses.bind(this)
-    this.classReady=this.classReady.bind(this)
-  }
-
-
-  updateClasses(cla){
-    this.setState({
-        ...this.state,
-        ini_data: {
-            classes: cla
-        }
-      });
-
-       setTimeout(() => {
-        console.log(this.state)
-    }, 10);
-  }
-
-  updateFrequency(fre) {
-    this.setState({
-        ...this.state,
-      ini_data: {
-        frequency: fre
-      }
+  function updateClasses(cla) {
+    setClasses(() => {
+      return cla;
     });
-    setTimeout(() => {
-        console.log(this.state)
-    }, 10);
+    setReady(false)
   }
 
-  classReady(){
-    let classes=this.state.ini_data.classes;
-    console.log(classes)
-    let comaClass;
-    let lowerClass=[],upperClass=[]
-
-    comaClass=classes.split(",");
-    console.log(comaClass)
-    comaClass.map((value)=>{
-        let valLowHigh=value.split("-")
-        console.log(valLowHigh)
-        lowerClass.push(parseInt(valLowHigh[0]))
-         upperClass.push(parseInt(valLowHigh[1]))
-
-        
-        return valLowHigh
-    })
-    console.log("lowerClass: ",lowerClass)
-    console.log("upperClass: ",upperClass)
-    
-    // setTimeout(() => {
-    //     console.log("upper :",this.state.data.classesUpper)
-    //     console.log("lower :",this.state.data.classesLower) 
-    // }, 1000);
-
-   
-        this.setState({
-            ...this.state,
-            data:{
-                classesUpper:upperClass,
-                classesLower: lowerClass,
-                tableClass:comaClass,
-            }
-        })
-    
-    setTimeout(() => {
-        console.log("classesUpper :",this.state.data.classesUpper)
-    console.log("classesLower :",this.state.data.classesLower)
-    }, 100);
-
-    let freQ=this.state.ini_data.frequency;
-    console.log("Frequency :",freQ)
-    console.log("Frequency :",freQ)
-    console.log("Frequency :",freQ)
-
-    // let newFrequency=freQ.split(",")
-    // newFrequency=newFrequency.map((value)=>parseInt(value))
-   
-
-    // let middlePoint=lowerClass.map((value,index)=>{
-    //     let upper=parseInt(upperClass[index])
-    //     let lower=parseInt(value)
-    //     let middle=(upper+lower)/2
-    //     return middle
-
-    // })
-
-    // this.setState({
-    //     ...this.state,
-    //     data:{
-    //         frequency:newFrequency,
-    //         middle:middlePoint,
-    //     }
-    // })
-
-    // setTimeout(() => {
-    //     console.log(this.state)
-    // }, 1000);
-
-//10-20,20-30,30-40,40-50
-//10,55,10,22
+  function updateFrequency(fre) {
+    setFrequency(() => {
+      return fre;
+    });
+    setReady(false)
   }
 
+  function classReady() {
+    //class work //class work//class work//class work//class work
+    console.log(classes);
+    console.log(frequency);
+    let viewClasses = classes.split(",");
+    setClasses(viewClasses)
+    let lowClass = viewClasses.map((val) => val.split("-")[0]);
+    lowClass = lowClass.map((value) => parseInt(value));
+    setLowClass(lowClass);
+    let highClass = viewClasses.map((val) => val.split("-")[1]);
+    highClass = highClass.map((value) => parseInt(value));
 
- 
-  render() {
-    return (
-      <div className="">
-        <Input
-          frequency={this.state.ini_data.frequency}
-          classes={this.state.ini_data.classes}
-          setStateF={this.updateFrequency}
-          setStateC={this.updateClasses}
-          send={this.classReady}
-        />
-        {this.state.table}
-      </div>
-    );
+    setHighClass(highClass);
+    let middlePoint = lowClass.map((value, index) => {
+      return (value + highClass[index]) / 2;
+    });
+    console.log(middlePoint);
+
+    setMidPoints(() => {
+      return middlePoint;
+    });
+    //class work//class work//class work//class work//class work
+
+    let numFre = frequency.split(",");
+    numFre = numFre.map((value) => parseInt(value));
+
+    setFrequency(() => {
+      return numFre;
+    });
+
+    //frequency work
+    //frequency work
+    //frequency work
+    //frequency work
+    setReady(true)
   }
+
+  //10-20,20-30,30-40,40-50
+  //10,55,10,22
+
+  return (
+    <div className="">
+      <Input
+        frequency={frequency}
+        classes={classes}
+        setStateF={updateFrequency}
+        setStateC={updateClasses}
+        send={classReady}
+      />
+      {ready?<Table
+        highClass={highClass}
+        lowClass={lowClass}
+        frequency={frequency}
+        classes={classes}
+        midPoints={midPoints}
+      />:0}
+    </div>
+  );
 }
 
 export default Main;
